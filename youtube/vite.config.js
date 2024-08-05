6b1d5921234634354d21abb30cc5f4e731f46302
Anirudh <anirudhvyas26@gmail.com>
2024-08-05 20:53:29 +0530

    updates

--- youtube/vite.config.js
+++ youtube/vite.config.js
@@ -1,18 +1,16 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
-import dotenv from "dotenv";
-
-// load environtment variables from .env file
-dotenv.config();
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
 
-  // const env = loadEnv(mode, process.cwd(), '')
+  const env = loadEnv(mode, process.cwd(), '')
   return {
     plugins: [react()],
     define: {
-      "process.env": process.env
+      "process.env": {
+        VITE_BACKEND_URL: env.VITE_BACKEND_URL
+      }
     },
     server: {
       proxy: {
