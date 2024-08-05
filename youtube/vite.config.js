bf993b1c6f76283729782d22dad4e9697db9425c
Anirudh <anirudhvyas26@gmail.com>
2024-08-05 19:36:15 +0530

    updates

--- youtube/vite.config.js
+++ youtube/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8800',
+        target: process.env.BACKEND_URL,
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
