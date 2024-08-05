eadc7bb306f231e4f4451629705e8400161dfa61
Anirudh <anirudhvyas26@gmail.com>
2024-08-05 20:30:15 +0530

    dotenv test

--- youtube/vite.config.js
+++ youtube/vite.config.js
@@ -1,14 +1,15 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
+import dotenv from dotenv;
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
 
-  const env = loadEnv(mode, process.cwd(), '')
+  // const env = loadEnv(mode, process.cwd(), '')
   return {
     plugins: [react()],
     define: {
-      __APP_ENV__: JSON.stringify(env.APP_ENV),
+      "process.env.VITE_BACKEND_URL": JSON.stringify(process.env.VITE_BACKEND_URL),
     },
     server: {
       proxy: {
