0527f3be4448d47cbb3f2cdbbedb88fa4420b69c
Anirudh <anirudhvyas26@gmail.com>
2024-08-05 20:08:35 +0530

    test

--- youtube/src/components/pages/watch/login/Login.jsx
+++ youtube/src/components/pages/watch/login/Login.jsx
@@ -57,6 +57,7 @@ export default function Login() {
           </small>
         </form>
       </div>
+      { JSON.stringify(process.env) }
     </div>
   );
 }
--- youtube/vite.config.js
+++ youtube/vite.config.js
@@ -1,17 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: process.env.VITE_BACKEND_URL,
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+export default defineConfig(({ command, mode }) => {
+
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    plugins: [react()],
+    define: {
+      __APP_ENV__: JSON.stringify(env.APP_ENV),
+    },
+    server: {
+      proxy: {
+        "/api": {
+          target: process.env.VITE_BACKEND_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ""),
+        },
       },
     },
-  },
+  };
 });
