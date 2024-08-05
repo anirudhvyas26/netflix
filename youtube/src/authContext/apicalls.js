0dd467f7e3627de3232be14e61043def3ff083b1
Anirudh <anirudhvyas26@gmail.com>
2024-08-05 19:52:26 +0530

    env updated

--- youtube/src/authContext/apicalls.js
+++ youtube/src/authContext/apicalls.js
@@ -7,7 +7,7 @@ export const login = async (user, dispatch) => {
   dispatch(loginStart());
 
   try {
-    const res = await axios.post(`/api/auth/login`, user);
+    const res = await axios.post(`${process.env.BACKEND_URL}/api/auth/login`, user);
 
     dispatch(loginSuccess(res.data));
   } catch (err) {
