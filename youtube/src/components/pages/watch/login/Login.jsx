682644d3b677aff0344c820b454c2a2637837868
Anirudh <anirudhvyas26@gmail.com>
2024-08-16 12:53:14 +0530

    login change

--- youtube/src/components/pages/watch/login/Login.jsx
+++ youtube/src/components/pages/watch/login/Login.jsx
@@ -21,7 +21,7 @@ export default function Login() {
         <div className="wrapper">
           <img
             className="logo"
-            src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png"
+           // src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png"
             alt=""
           />
         </div>
@@ -46,7 +46,7 @@ export default function Login() {
             Sign In
           </button>
           <span>
-            New to Netflix?
+            New to Aniflix Project?
             <Link to="/register">
               <b>Sign up now</b>
             </Link>
